bdaa514906bb4a6dcd95b92a109d6d182c3bc69b
Gonçalo Ferreira Silva <goncalo.silva.22679@formandos.cinel.pt>
2022-09-19 16:11:51 +0100

    Helperes

---
+++ ClínicaVeterináriaGS/Helperes/IUserHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using VeterinaryClinicGS.Data.Entities;
+using System.Threading.Tasks;
+using SuperShopGS.Models;
+
+namespace VeterinaryClinicGS.Helperes
+{
+    public interface IUserHelper
+    {
+        Task<User> GetUserByEmailAsync(string email);
+
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+
+        Task<SignInResult> LoginAsync(LoginViewModel model);
+
+        Task LogoutAsync();
+
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+
+
+
+
+
+
+
+
+
+
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task <bool> IsUserInRoleAsync(User user, string roleName);
+    }
+}
---
+++ ClínicaVeterináriaGS/Helperes/NotFoundViewResult.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace VeterinaryClinicGS.Helperes
+{
+    public class NotFoundViewResult : ViewResult
+    {
+        public NotFoundViewResult(string viewName)
+        {
+            ViewName = viewName;
+            StatusCode = (int)HttpStatusCode.NotFound;
+        }
+
+
+
+
+    }
+}
